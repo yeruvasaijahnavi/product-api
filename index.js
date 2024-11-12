6fa82a25662ae54a88d9484cdbe53e65c5d52293
Yeruva Sai Jahnavi <77843177+yeruvasaijahnavi@users.noreply.github.com>
2024-11-12 13:04:59 +0530

    Add product data and implement sorting functionality for products endpoint

--- index.js
+++ index.js
@@ -11,7 +11,38 @@ let products = [];
 
 // get all prodcuts
 app.get("/products", (req, res) => {
-	res.status(200).json(products);
+	const { sortBy, order } = req.query;
+	const productsCopy = [...products];
+	console.log("sortBy:", sortBy, "order:", order);
+	if (sortBy) {
+		if (sortBy === "price") {
+			console.log("sorting by price");
+			productsCopy.sort((p1, p2) => {
+				if (order === "asc") return p1.price - p2.price;
+				else if (order === "desc") return p2.price - p1.price;
+				else {
+					res.status(400).json({
+						error: "Invalid order value, should be asc or desc",
+					});
+				}
+			});
+		} else if (sortBy === "name") {
+			console.log("sorting by name");
+			productsCopy.sort((p1, p2) => {
+				if (order === "asc") return p1.name.localeCompare(p2.name);
+				else if (order === "desc")
+					return p2.name.localeCompare(p1.name);
+				else {
+					res.status(400).json({
+						error: "Invalid order value, should be asc or desc",
+					});
+				}
+			});
+		} else {
+			res.status(400).json({ error: "Invalid sortBy value" });
+		}
+	}
+	res.status(200).json(productsCopy);
 });
 
 // single prod by id
@@ -71,7 +102,7 @@ app.delete("/products/:id", (req, res) => {
 
 	if (productIndex !== -1) {
 		products.splice(productIndex, 1); // remove '1' elements starting from productIndex
-		res.status(204).send();
+		res.status(204).send("Product deleted");
 	} else {
 		res.status(404).json({ error: "Product not found" });
 	}
